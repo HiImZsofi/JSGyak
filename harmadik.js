e39ac3782cb9dd3912c63bfec8ba0324517d2ab5
HiImZsofi <zsofia.gyori00@gmail.com>
2022-09-08 12:40:36 +0200

    task three done

---
+++ harmadik.js
@@ -0,0 +1,16 @@
+setInterval(function () {
+    console.log("Hurrá!")
+}, 2000);
+
+let szam = 0;
+setInterval(function () {
+    console.log(szam)
+    szam++
+}, 2000);
+
+$( document ).ready(function() {
+    setInterval(function () {
+        console.log("Hurrá!")
+    }, 2000);
+});
+
